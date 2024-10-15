ed32244dd6692f6dde6f479533b9e35c902efa38
tech-gecko <w0nderzz2001@gmail.com>
2024-10-15 15:42:44 +0100

    Add task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """This module contains an asynchronous coroutine that takes in an integer
-argument (max_delay, with a default value of 10) named wait_random that waits
+argument (max_delay, with a default value of 10) named "wait_random" that waits
 for a random delay between 0 and max_delay (included and float value) seconds
 and eventually returns it."""
 
@@ -9,7 +9,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """waits for a random delay between 0 and max_delay
+    """Waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""This module imports "wait_random" from 0-basic_async_syntax.py and writes an
+async routine called "wait_n" that takes in 2 int arguments (in this order):
+n and max_delay. wait_n will spawn wait_random n times with the specified
+max_delay. wait_n would return the list of all the delays (float values).
+The list of the delays would be in ascending order."""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spawns "wait_random" n times with the specified max_delay. Returns the
+    list of all the delays (float values). The list of the delays would be in
+    ascending order."""
+    return_list = []
+
+    for i in range(n):
+        delay = await wait_random(max_delay=max_delay)
+
+        inserted = False
+        for j in range(len(return_list)):
+            if delay < return_list[j]:
+                return_list.insert(j, delay)
+                inserted = True
+                break
+
+        if not inserted:
+            return_list.append(delay)
+
+    return return_list
