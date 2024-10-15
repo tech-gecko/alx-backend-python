5993c0b7f9276994c242f37e0965087b10d47f7c
tech-gecko <w0nderzz2001@gmail.com>
2024-10-15 15:58:47 +0100

    Change return type from list[float] to List[float]

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,13 +4,12 @@ async routine called "wait_n" that takes in 2 int arguments (in this order):
 n and max_delay. wait_n will spawn wait_random n times with the specified
 max_delay. wait_n would return the list of all the delays (float values).
 The list of the delays would be in ascending order."""
-
-import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns "wait_random" n times with the specified max_delay. Returns the
     list of all the delays (float values). The list of the delays would be in
     ascending order."""
