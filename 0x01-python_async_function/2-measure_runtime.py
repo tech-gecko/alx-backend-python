a597721c270a164d3e769a3b85fe658da8f9f428
tech-gecko <w0nderzz2001@gmail.com>
2024-10-15 16:56:18 +0100

    Update task 2 to use asyncio.run() instead of 'await'

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,6 +3,7 @@
 function called "measure_time" that takes in 2 int arguments, n and max_delay.
 measure_time will measure the total execution time for wait_n(n, max_delay) and
 would return the total time divided by n (the average delay)."""
+import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -12,7 +13,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     """ Measures the total execution time for wait_n(n, max_delay), and
     returns total_time / n."""
     start_time = time.perf_counter()
-    await wait_n(n=n, max_delay=max_delay)
+    asyncio.run(wait_n(n=n, max_delay=max_delay))
     total_time = time.perf_counter() - start_time
 
     return total_time / n  # Returns average delay.
