d4e5a187d1c916348f2a4aa9d8858c00a3ea276b
tech-gecko <w0nderzz2001@gmail.com>
2024-10-15 14:27:21 +0100

    Add module documentation.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""This module contains an asynchronous coroutine that takes in an integer
+argument (max_delay, with a default value of 10) named wait_random that waits
+for a random delay between 0 and max_delay (included and float value) seconds
+and eventually returns it."""
 
 import asyncio
 import random
